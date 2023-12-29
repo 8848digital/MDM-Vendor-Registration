fa66718341ae36a8f41fc9ecadeffb59e00cfcad
rushikesh.p@8848digital.com <8848rushikesh>
2023-12-29 17:04:25 +0530

    changed Supplier Company doctype name

--- mdm_vendor_registration/mdm_vendor_registration/doctype/supplier_company_details/supplier_company_details.js
+++ mdm_vendor_registration/mdm_vendor_registration/doctype/supplier_company_details/supplier_company_details.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, chintan and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Company Details', {
+frappe.ui.form.on('Supplier Company Details', {
 	// refresh: function(frm) {
 
 	// }
