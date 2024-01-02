35d389321c924c673985716d3cac694ba0e95825
rushikesh.p@8848digital.com <8848rushikesh>
2024-01-02 12:42:30 +0530

    Supplier Address and Verification Checkboxes Plus Supplier invitation Doctype

---
+++ mdm_vendor_registration/mdm_vendor_registration/doctype/supplier_invitation/supplier_invitation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, chintan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Supplier Invitation", {
+// 	refresh(frm) {
+
+// 	},
+// });
