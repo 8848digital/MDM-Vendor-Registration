e22061c95217d52986b746905f1e849f7d98bb4f
rahulgupta8848 <rahul.gupta@8848digital.com>
2024-01-05 10:44:23 +0530

    conflict

--- mdm_vendor_registration/mdm_vendor_registration/web_form/new_web_form_rkznyeaxdt/new_web_form_rkznyeaxdt.js
+++ mdm_vendor_registration/mdm_vendor_registration/web_form/new_web_form_rkznyeaxdt/new_web_form_rkznyeaxdt.js
@@ -1,8 +1,12 @@
 frappe.ready(function() {
+
 var customBtn = $('<button class="custom-btn btn btn-default" style="font-size: small;margin-left:10px; padding-left: 5px; padding-right: 5px;">Verify PAN</button>');
 var panLabel = $('.label-area:contains("PAN Applicable")');
 var labelContainer = panLabel.parent();
 labelContainer.append(customBtn);
+frappe.web_form.set_value('pan_applicable',1);
+frappe.web_form.set_value('pan_applicable',0);
+
 $('.custom-btn').on('click', function(event) {
 	event.preventDefault();
 	frappe.call({
@@ -10,6 +14,8 @@ $('.custom-btn').on('click', function(event) {
 		callback: function(response) {
 			var fetchCompanyNameBasedOnPan = response.message;
 			if (fetchCompanyNameBasedOnPan == 1) {
+				frappe.web_form.set_value('pan_verified',1);
+
 				var pan_number = frappe.web_form.get_value('pan_number');
 				if (pan_number) {
 					frappe.call({
@@ -18,11 +24,14 @@ $('.custom-btn').on('click', function(event) {
 							pan_number: pan_number
 						},
 						callback: function(response) {
+							console.log(response.message)
 							if (response.message === false) {
 								frappe.msgprint("PAN number not valid");
 							} 
 							else {
 								var data= response.message.data;
+							
+							
 								frappe.web_form.set_value('persons_name', data.full_name);
 								frappe.web_form.set_value('company_type_as_per_pan', data.category);
 							}
